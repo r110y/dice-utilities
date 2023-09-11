383eeeaa3f74fd1118c3bd5fee27c181e2649083
rhysplloyd <79843065+rhysplloyd@users.noreply.github.com>
2023-09-11 19:50:49 +0100

    Refactored rollDiceObject

--- index.js
+++ index.js
@@ -11,47 +11,38 @@ function rollDice(sides = 6, count = 1) {
 
 // 2. Pass the number of faces and a count to recieve an object.
 
-function rollDiceAsObject(sides, count = 1) {
+function rollDiceObject(sides = 6, count = 1) {
+  // Handle errors
   if (count < 1 || sides < 1) return;
+  if (!Number.isInteger(count) || !Number.isInteger(sides)) return;
 
-  const info = {
-    type: `D${sides}`,
-    rolls: Array.from(
-      { length: count },
-      () => Math.floor(Math.random() * sides) + 1
-    ),
-    getSum: function () {
-      return this.rolls.reduce((total, roll) => total + roll, 0);
-    },
-    getMin: function () {
-      return Math.min(...this.rolls);
-    },
-    getMax: function () {
-      return Math.max(...this.rolls);
-    },
-    getAbilityScore: function () {
-      if (count <= 1) return;
-      const sum = this.rolls.reduce((total, roll) => total + roll, 0);
-      const min = Math.min(...this.rolls);
+  // Generate values
+  const rolls = Array.from(
+    { length: count },
+    () => Math.floor(Math.random() * sides) + 1
+  );
 
-      return sum - min;
-    },
+  // Calculate values of attributes
+  const min = Math.min(...rolls);
+  const max = Math.max(...rolls);
+  const sum = rolls.reduce((total, roll) => total + roll, 0);
+  const takeLowest = () => {
+    if (count < 1) return undefined;
+    return max - min;
   };
 
-  return info;
+  // Return object from calculated values
+  return {
+    type: `D${sides}`,
+    rolls: rolls,
+    min: min,
+    max: max,
+    sum: sum,
+    takeLowestValue: takeLowest,
+  };
 }
 
-// 3. Get an ability score using the 'take-lowest' method. This function relies on rollDiceAsObject
+// 3. Get a 5e ability score from a dice object.
 
-function getAbilityScore() {
-  const roll = rollDiceAsObject(6, 4);
-
-  const final = roll.getSum() - roll.getMin();
-  console.log(final);
-
-  return roll.getSum() - roll.getMin();
-}
-
-// Or use the built-in method
-
-rollDiceAsObject(6, 4).getAbilityScore();
+const abilityScore = rollDiceObject(6, 4);
+console.log(abilityScore);
